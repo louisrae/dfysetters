37de05eeebda1d29822c76be13065fa6c7cc7c04
louisrae <87879917+louisrae@users.noreply.github.com>
2022-03-02 09:10:32 -0600

    Iterate version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="dfysetters",
-    version="2.0.1",
+    version="2.0.2",
     description="Back end code base for a sales-as-a-service business",
     long_description=long_description,
     long_description_content_type="text/markdown",
